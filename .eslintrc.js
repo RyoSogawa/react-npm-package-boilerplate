4f63f9c39543baa7d3a70e7ebaf9fafb84a5c370
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-26 21:13:39 +0900

    add airbnb eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'jsx-a11y', 'react', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  settings: {
+    react: {
+      version: '18',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 2,
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'object-shorthand': ['error', 'properties'],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-restricted-exports': 'off',
+    'consistent-return': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.test.tsx'],
+      rules: {
+        'max-lines': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+}
